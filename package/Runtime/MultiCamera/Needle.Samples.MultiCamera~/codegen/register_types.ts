ffde8fdceb0a014b3fad7306d6328a0f54e22b22
hybridherbst <felix.herbst@gmail.com>
2023-12-11 17:30:31 +0100

    add multicamera scissor sample

---
+++ package/Runtime/MultiCamera/Needle.Samples.MultiCamera~/codegen/register_types.ts
@@ -0,0 +1,7 @@
+﻿import { TypeStore } from "@needle-tools/engine"
+
+// Import types
+import { CameraScissor } from "../CameraScissor.js";
+
+// Register types
+TypeStore.add("CameraScissor", CameraScissor);
